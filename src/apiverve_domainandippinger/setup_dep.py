0f15d0e959cdde6401ad0bde0f58c57e76785510
APIVerve Team <hello@apiverve.com>
2025-03-28 17:41:03 -0500

    Bug Fixes

---
+++ src/apiverve_domainandippinger/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_domainandippinger',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Pinger is a simple tool for checking if a URL is up. It returns a boolean value indicating whether the URL is up or not.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
